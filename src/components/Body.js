08211eb3e2a900f9af3154a52fe41cb88af84e8a
Viacheslav <1.slava.1.amelin.1@gmail.com>
2023-05-19 15:49:05 +0300

    fix: new navigation and added new folders for labs

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,73 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 import '../css/style.css';
 
-// Lab 1
-import Page1 from '../lab1/Page1';
-import Page2 from '../lab1/Page2';
-import Page3 from '../lab1/Page3';
-import Page4 from '../lab1/Page4';
-import Page5 from '../lab1/Page5';
+const loadPage = (labNumber, pageNumber) => {
+  return React.lazy(() => import(`../lab${labNumber}/Page${pageNumber}`));
+}
 
 function Body({ lab }) {
   const [page, setPage] = useState(0);
 
-  const labs = [
-    { 
-      name: 'Lab 1', 
-      pages: [<Page1 />, <Page2 />, <Page3 />, <Page4 />, <Page5 />], 
-      buttonNames: ['Тема, Мета, Місце розташування звітного HTML-документу', 'Опис предметного середовища', 'Зовнішній вигляд головної сторінки', 'HTML-код головної сторінки', 'ВИСНОВКИ'] 
-    }, 
-    { 
-      name: 'Lab 2', 
-      pages: [<Page1 />, <Page2 />, <Page3 />], 
-      buttonNames: ['Тема, Мета, Місце розташування звітного HTML-документу', 'Способи підключення стилів', 'Селектори', 'CSS: шрифти, текст, фон, контур', 'ВИСНОВКИ']  
-    }, 
-    { 
-      name: 'Lab 3', 
-      pages: [<Page1 />, <Page2 />, <Page3 />], 
-      buttonNames: ['Тема, Мета, Місце розташування звітного HTML-документу', 'ЗАВДАННЯ №1 (зовнішній вигляд макету, код макету)', 'ЗАВДАННЯ №2', 'ЗАВДАННЯ №3 FLEXBOX', 'ВИСНОВКИ']  
-    }, 
-    { 
-      name: 'Lab 4', 
-      pages: [<Page1 />, <Page2 />, <Page3 />], 
-      buttonNames: ['Тема, Мета, Місце розташування звітного HTML-документу', 'Способи функціонального застосування JavaScript', 'Масиви (пункт4)', 'Виконання пункту 6', 'ВИСНОВКИ']  
-    },
-    { 
-      name: 'Lab 5', 
-      pages: [<Page1 />, <Page2 />, <Page3 />], 
-      buttonNames: ['Тема, Мета, Місце розташування звітного HTML-документу', 'Динамічні елементи третього пункту', 'Галерея UniteGallery', 'Кнопки соціальних мереж', 'Інформери', 'ВИСНОВКИ']  
-    },
-    { 
-      name: 'Lab 6', 
-      pages: [<Page1 />, <Page2 />, <Page3 />], 
-      buttonNames: ['Button 1', 'Button 2', 'Button 3']  
-    },
-    { 
-      name: 'Lab 7', 
-      pages: [<Page1 />, <Page2 />, <Page3 />], 
-      buttonNames: ['Button 1', 'Button 2', 'Button 3']  
-    },
-    { 
-      name: 'Lab 8', 
-      pages: [<Page1 />, <Page2 />, <Page3 />], 
-      buttonNames: ['Button 1', 'Button 2', 'Button 3']  
-    },
-    { 
-      name: 'Lab 9', 
-      pages: [<Page1 />, <Page2 />, <Page3 />], 
-      buttonNames: ['Button 1', 'Button 2', 'Button 3']  
-    },
-  ];
+  const [Page, setPageComponent] = useState(null);
   
+  useEffect(() => {
+    setPageComponent(loadPage(lab + 1, page + 1));
+  }, [lab, page]);
 
   const handlePageChange = (index) => {
     setPage(index);
   };
 
-  const contentPage = (page) => {
-    return <div>{page}</div>;
-  }
-
+  const labs = [
+    { name: 'Lab 1', buttonNames: ['Тема, Мета, Місце розташування звітного HTML-документу', 'Опис предметного середовища', 'Зовнішній вигляд головної сторінки', 'HTML-код головної сторінки', 'ВИСНОВКИ'] },
+    { name: 'Lab 2', buttonNames: ['Button 1', 'Button 2', 'Button 3', 'Button 4', 'Button 5'] },
+    { name: 'Lab 3', buttonNames: ['Button 1', 'Button 2', 'Button 3', 'Button 4', 'Button 5'] },
+    { name: 'Lab 4', buttonNames: ['Button 1', 'Button 2', 'Button 3', 'Button 4', 'Button 5'] },
+    { name: 'Lab 5', buttonNames: ['Button 1', 'Button 2', 'Button 3', 'Button 4', 'Button 5'] },
+    { name: 'Lab 6', buttonNames: ['Button 1', 'Button 2', 'Button 3', 'Button 4', 'Button 5'] },
+    { name: 'Lab 7', buttonNames: ['Button 1', 'Button 2', 'Button 3', 'Button 4', 'Button 5'] },
+    { name: 'Lab 8', buttonNames: ['Button 1', 'Button 2', 'Button 3', 'Button 4', 'Button 5'] },
+    { name: 'Lab 9', buttonNames: ['Button 1', 'Button 2', 'Button 3', 'Button 4', 'Button 5'] },
+  ];
+  
   return (
     <div className="body__wrapper">
       <div className='body__aside'>
@@ -81,7 +43,9 @@ function Body({ lab }) {
         </div>
       </div>
       <div className='body__content'>
-        {contentPage(labs[lab].pages[page])}
+        <Suspense fallback={<div>Loading...</div>}>
+          {Page && <Page />}
+        </Suspense>
       </div>
     </div>
   );
---
+++ src/lab2/Page3.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import lab1 from '../img/lab1.png'
+
+function Page3() {
+  return <div>
+    <img src={lab1}/>
+  </div>;
+}
+
+export default Page3;
