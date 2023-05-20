eabf0334fa41f8e3ce2fb60a87ce6c7cd58eb09f
Nikolai <nikolaichub.me@gmail.com>
2023-05-20 23:44:26 +0300

    feat: lab4 chub 1,2

--- src/lab4/Page4.js
+++ src/lab4/Page4.js
@@ -1,8 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Page4() {
+  const [inputArray, setInputArray] = useState([]);
+  const [inputArray2, setInputArray2] = useState([]);
+  const [min, setMin] = useState(0);
+  const [arraySize, setArraySize] = useState('');
+
+  const handleInputChange = (e) => {
+    setArraySize(e.target.value);
+  };
+  
+  const buildArrays = () => {
+    const size = parseInt(arraySize);
+    const newArray = [];
+    const newArray2 = [];
+    for (let i = 0; i < size; i++) {
+      const randomValue = Math.floor(Math.random() * 10);
+      const randomValue2 = Math.floor(Math.random() * 10);
+      newArray.push(randomValue);
+      newArray2.push(randomValue2);
+    }
+    setInputArray(newArray);
+    setInputArray2(newArray2);
+  };
+
+  const findCommonMin = () => {
+    let minvalue = inputArray[0];
+    let intersection = inputArray.filter(x => inputArray2.includes(x));
+    console.log(intersection);
+    for (const el of intersection) {
+        if (el < minvalue) minvalue = el;
+    }
+    setMin(minvalue);
+  }
+
     return (
-        <div>Koll</div>
+        <div>
+            <h3>Введіть розмір масивів:</h3>
+            <input
+                type="number"
+                value={arraySize}
+                onChange={handleInputChange}
+                placeholder="Введіть кількість елементів масиву А"
+            />
+            <button onClick={buildArrays}>Побудувати масиви</button>
+            <h3>Вхідний масив А:</h3>
+            <p>{inputArray.join(', ')}</p>
+
+            <h3>Вхідний масив В:</h3>
+            <p>{inputArray2.join(', ')}</p>
+
+            <button onClick={findCommonMin}>Найменший елемент</button>
+            <p>{min}</p>
+        </div>
     );
 }
 
--- src/lab4/Page7.js
+++ src/lab4/Page7.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Page7() {
+    const [time, setTime] = useState();   
+    const [date, setDate] = useState();
+    const [alarm, setAlarm] = useState();
+    const [started, setAlarmStart] = useState(false);
+
+    const handleTime = (e) => {
+        setTime(e.target.value);
+    }
+
+    const handleDate = (e) => {
+        setDate(e.target.value);
+    }
+
+    const handleAlarm = () => {
+        const alarm = new Date(`${date}T${time}:00`).getTime();
+        setAlarmStart(true);
+        setAlarm(alarm);
+        const timer = setInterval(() => {
+            if (Date.now() >= alarm) {
+                alert('Будильник!');
+                clearInterval(timer);
+                setAlarmStart(false);
+            }
+        }, 1000);
+    }
+
     return (
-        <div>Koll</div>
+        <div>
+            <h3>Введіть час:</h3>
+            <input onChange={handleTime} type='time'/>
+
+            <h3>Введіть дату:</h3>
+            <input onChange={handleDate} type='date'/><br />
+
+            <button onClick={handleAlarm} disabled={started}>Поставити будильник</button>
+        </div>
     );
 }
 
