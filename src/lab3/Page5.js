7e21fc74ec3e47122ea884ccbda698230937220b
Artem <artempozitiv56@gmail.com>
2023-05-20 17:09:43 +0200

    feat: added lab3 Krutikov

--- src/lab3/Page5.js
+++ src/lab3/Page5.js
@@ -1,9 +1,87 @@
-import React from 'react';
+import React from "react";
+import image1 from './KrutikovTask3/1.png';
+import image2 from './KrutikovTask3/2.png';
+import image3 from './KrutikovTask3/3.png';
+import image4 from './KrutikovTask3/4.png';
+import image5 from './KrutikovTask3/5.png';
+import image6 from './KrutikovTask3/6.png';
+import image7 from './KrutikovTask3/7.png';
+import image8 from './KrutikovTask3/8.png';
 
-function Page5() {
-  return <div>
-    <strong>Висновок: </strong>Під час виконання лабораторної роботи було придбано практичні навички роботи верстки сторінок засобами CSS, верстки на основі плаваючих елементів, з’ясовано переваги та недоліки типів макетів веб-сторінок, а саме створення веб-додатків за допомогою таблиці є простішим але менш гнучким, особливо для додатків зі складнішою логікою, а за допомогою блоків навпаки, вони краще підходять для складних та інтеративних додатків та придбано практичні навички роботи верстки сторінок засобами CSS та FLEXBOX.
-    </div>;
-}
+const App = () => {
+  return (
+    <div className="">
+    <div className="course">
+    <h3>ПОПУЛЯРНІ НАПРЯМКИ</h3>
+    <h2>КУРСІВ ПРОГРАМУВАННЯ</h2>
+    <div className="container">
+        <div className="good">
+          <img src={image1} width="100px" height="100px" alt=""></img>
+            <h4>javascript</h4>
+            <h5>Комплексний курс</h5>
+            <p>Стартовий рівень: початківець<br></br>
+                Тривалість: 5-11 місяців<br></br>
+                Ціна: від 14500грн.</p>
+            <span>Залишилося 3 місця</span>
+            <hr></hr>
+            <a href="#">Детальніше</a>
+        </div>
 
-export default Page5;
+        <div className="good">
+          <img src={image2}width="100px" height="100px" alt=""></img>
+            <h4>javascript</h4>
+            <h5>Комплексний курс</h5>
+            <p>Стартовий рівень: початківець<br></br>
+                Тривалість: 6-11 місяців<br></br>
+                Ціна: від 14500грн.</p>
+            <span>Залишилося 6 місця</span>
+            <hr></hr>
+            <a href="#">Детальніше</a>
+        </div>
+        <div className="good">
+          <img src={image3} width="100px" height="100px" alt=""></img>
+            <h4>javascript</h4>
+            <h5>Комплексний курс</h5>
+            <p>Стартовий рівень: початківець<br></br>
+                Тривалість: 6-11 місяців<br></br>
+                Ціна: від 14500грн.</p>
+            <span>Залишилося 4 місця</span>
+            <hr></hr>
+            <a href="#">Детальніше</a>
+        </div>
+        </div>
+    </div>
+
+    <div className="wrapper_item">
+      <img src={image4}alt=""></img>
+      <img src={image5}alt=""></img>
+      <img src={image6}alt=""></img>
+      <img src={image7}alt=""></img>
+    </div>
+
+    <div className="wrapper_pro">
+    <img src={image8}alt=""></img>
+    <h3>ВІД НОВАЧКА ДО PRO-РОЗРОБНИКА</h3>
+    <p className="pro_txt">Робота в ІТ - це високі зарплати, можливість
+        віддаленої співпраці із закордонними компаніями і
+        свобода вибору. Але є мінуси. Низький старт -
+        головний з них. Фахівцю без досвіду складно
+        отримати робочий стаж, а зарплата коливається на
+        рівні мінімальної.<br></br>
+        Накурсахз інкубатором наші студенти
+        отримують додаткових 5 місяців досвіду
+        який так сильно вимагають  роботодааці.<br></br>
+        Зттаким резюме ви станете бажаним
+        кандидатом для будь-якої компанії.</p>
+    </div>
+
+    <footer>
+      <p className="footer_desc">НАВЧАЙСЯ ЗАРАЗ, СПЛАЧУЙ ПОТІМ 
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <span>РОЗТЕРМІНУВАННЯ НА 24 МІСЯЦІ</span></p>
+    </footer>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/lab3/Page6.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Page6() {
+  return <div>
+    ...
+    </div>;
+}
+
+export default Page6;
