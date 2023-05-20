9c36bc477f9d920645c90276a6bca51e7ffade65
Nikolai <nikolaichub.me@gmail.com>
2023-05-21 00:57:54 +0300

    feat: lab5 task 4-5

--- src/lab5/Page3.js
+++ src/lab5/Page3.js
@@ -1,9 +1,22 @@
-import React, { useEffect } from 'react';
-
+import React from 'react';
+import img1 from './social.png'
 function Page3() {
-  return(
-    <div>Page 3</div>
-  )
+  const code = `
+  <div class="social-links">
+    <a href="#" class="social-link"><img src="img/instagram.svg" alt="instagram" /></a>
+    <a href="#" class="social-link"><img src="img/facebook.svg" alt="facebook" /></a>
+    <a href="#" class="social-link"><img src="img/twitter.svg " width="25px" alt="twitter" /></a>
+  </div>
+  `;
+  
+  return (  
+    <pre>
+      <img src={img1}/>
+      <pre>
+        {code}
+      </pre>
+    </pre>
+  );
 }
 
 export default Page3;
