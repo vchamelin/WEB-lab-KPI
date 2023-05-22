73c7e4e8e259fd10108c9eb647bd5fcab955b2dd
Artem <artempozitiv56@gmail.com>
2023-05-22 15:51:37 +0200

    feat: added links

--- src/lab3/Page3.js
+++ src/lab3/Page3.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Page3() {
   return <div>
     <ol>
-      <li><a href='#'>Фіксована таблична верстка</a></li>
-      <li><a href='#'>Гумова таблична верстка</a></li>
-      <li><a href='#'>Фіксована блокова верстка</a></li>
-      <li><a href='#'>Гумова блокова верстка</a></li>
+      <li><a href='https://cmertenok.github.io/fe-labs/lab3/task2/table_fixed.html'>Фіксована таблична верстка</a></li>
+      <li><a href='https://cmertenok.github.io/fe-labs/lab3/task2/table_rubber.html'>Гумова таблична верстка</a></li>
+      <li><a href='https://cmertenok.github.io/fe-labs/lab3/task2/block_fixed.html'>Фіксована блокова верстка</a></li>
+      <li><a href='https://cmertenok.github.io/fe-labs/lab3/task2/block_rubber.html'>Гумова блокова верстка</a></li>
     </ol>
   </div>;
 }
